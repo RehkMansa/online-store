775bc837c5dc3b12c206a8f93c5888519f1104a6
RehkMansa <godspowernathaniel25@gmail.com>
2022-06-09 22:04:36 +0100

    added global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import GlobalStyles from './components/styles/Global';
+import styledComponents from 'styled-components';
+
+const RoutesContainer = styledComponents.main`
+
+`
+
 function App() {
   return (
     <div className="App">
-      <Header />
+      <GlobalStyles />
+      <Navbar />
+      <RoutesContainer>
+        <h1>Routes go in here</h1>
+      </RoutesContainer>
       <Footer />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,10 @@
+import React from 'react';
 const Navbar = () => {
   return (
     <nav>
       <div>
         <img src="images/logo.svg" alt="logo" />
       </div>
-      <ul>
-        <li>
-          <a>Home</a>
-        </li>
-        <li>
-          <a>Login</a>
-        </li>
-        <li>
-          <a>Store</a>
-        </li>
-      </ul>
     </nav>
   );
 };
